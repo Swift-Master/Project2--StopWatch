7369ddd36fc2b66de28c9bcc36112aa820b9fed0
AKAPUCH <chldnxo5@naver.com>
2023-04-26 23:16:28 +0900

    코드 리팩토링

--- StopwatchOfClock/StopwatchOfClock/BackgroundTimer.swift
+++ StopwatchOfClock/StopwatchOfClock/BackgroundTimer.swift
@@ -1,20 +1,25 @@
 import Foundation
 
-class BackgroundTimer {
+// MARK: - DispatchSourceTimer를 구현한 클래스
+final class BackgroundTimer {
+    
+    // MARK: - 상태 관리를 위해 열거형을 정의
     private enum State {
         case suspended
         case resumed
     }
     private var state: State = .suspended
-    private var timerLock = NSLock()
+    private var timerLock = NSLock() // 동시성 문제를 해결하기 위해 도입
     private var interval: Int
     private var eventHandler: (() -> Void)?
-
+    
+    // MARK: - Memberwise 생성자를 통해 생성과 동시에 handler를 설정할 수 있습니다.
     init(with interval: Int, handler: (() -> Void)? = nil) {
         self.interval = interval
         self.eventHandler = handler
     }
     
+    // MARK: - main이 아닌 custom 디스패치 대기열을 생성합니다.(기본: concurrent)
     private lazy var timer: DispatchSourceTimer = {
         let t = DispatchSource.makeTimerSource(flags: [], queue: DispatchQueue.init(label: "thanks.codesquad.timer"))
         t.schedule(deadline: .now(), repeating: .milliseconds(self.interval))
@@ -24,6 +29,7 @@ class BackgroundTimer {
         return t
     }()
     
+    // MARK: - 추가 인스턴스를 생성하지 않고 기존 타이머 객체를 재활용합니다.
     func reschedule(interval: Int, handler: (() -> Void)? = nil) {
         self.interval = interval
         timer.schedule(deadline: DispatchTime.now(), repeating: .milliseconds(interval))
@@ -32,7 +38,8 @@ class BackgroundTimer {
             self?.eventHandler?()
         })
     }
-
+    
+    // MARK: - 타이머에 설정된 핸들러를 제거한 후 타이머를 종료시킵니다.
     deinit {
         timer.setEventHandler(handler: nil)
         timer.cancel()
--- StopwatchOfClock/StopwatchOfClock/ViewController.swift
+++ StopwatchOfClock/StopwatchOfClock/ViewController.swift
@@ -1,17 +1,12 @@
-//
-//  ViewController.swift
-//  StopwatchOfClock
-//
-//  Created by 최우태 on 2023/04/21.
-//
-
 import UIKit
 
 final class ViewController: UIViewController {
     
     var totalTimer : BackgroundTimer?
     var lapTimer : BackgroundTimer?
-    var lapTimes : [String?] = []
+    
+    var lapTimeList : [String?] = [] // 랩타임 저장 및 테이블뷰의 참조 데이터
+    
     var totalTime : Double = 0.00
     var lapTime : Double = 0.00
     
@@ -19,101 +14,121 @@ final class ViewController: UIViewController {
     
     @IBOutlet weak var lapTimeLabel: UILabel!
     
-    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
     
-    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var lapResetButton: UIButton!
     
-    @IBOutlet weak var laptimeRecordTable: UITableView!
+    @IBOutlet weak var laptimeRecordTableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        InitializedOnce()
+    }
+    
+    func InitializedOnce() {
         totalTimeLabel.text = "00:00.00"
         lapTimeLabel.text = "00:00.00"
-        // Do any additional setup after loading the view.
+        lapResetButton.isEnabled = false
     }
-
-    @IBAction func clickStartButton(_ sender: UIButton) {
-
-        if startButton.titleLabel?.text == "Start" {
-            startButton.setTitle("Stop", for: .normal)
-            startButton.setTitleColor(.systemRed, for: .normal)
-            recordButton.isEnabled = true
-            recordButton.setTitle("Lap", for: .normal)
-            
-            // 각 라벨에 시간 기록 시작
-            if totalTimer == nil {
-                totalTimer = BackgroundTimer(with: 10,handler: {[weak self] in
-                    guard let self = self else {return}
-                    self.totalTime += 0.01
-                    let minute = Int(self.totalTime) / 60
-                    let second = self.totalTime - Double(minute) * 60
-                    DispatchQueue.main.async {
-                        self.totalTimeLabel.text = String(format:"%02d:%05.2f",minute,second)
-                    }
-                })
-                lapTimer = BackgroundTimer(with: 10,handler: {[weak self] in
-                    guard let self = self else {return}
-                    self.lapTime += 0.01
-                    let minute = Int(self.lapTime) / 60
-                    let second = self.lapTime - Double(minute) * 60
-                    DispatchQueue.main.async {
-                        self.lapTimeLabel.text = String(format:"%02d:%05.2f",minute,second)
-                    }
-                })
-                totalTimer?.activate()
-                lapTimer?.activate()
-            }else {
-                totalTimer?.resume()
-                lapTimer?.resume()
-            }
-        }else {
-            startButton.setTitle("Start", for: .normal)
-            startButton.setTitleColor(.systemGreen, for: .normal)
-            recordButton.setTitle("Reset", for: .normal)
-            // 시간 기록 정지
-            totalTimer?.suspend()
-            lapTimer?.suspend()
+    
+    func setTimeLabel(_ time : Double, _ targetLabel : UILabel) {
+        let minute = Int(time) / 60
+        let second = time - Double(minute) * 60
+        DispatchQueue.main.async {
+            targetLabel.text = String(format:"%02d:%05.2f",minute,second)
         }
     }
     
-    @IBAction func clickRecordButton(_ sender: UIButton) {
-        
-        if recordButton.titleLabel?.text == "Lap" {
-            //테이블 뷰에 랩타입 기록
-            // 랩타임 기록 라벨 00:00.00으로 초기화
-            lapTimer?.suspend()
-            DispatchQueue.main.async {[self] in
-                let minute = Int(lapTime) / 60
-                let second = lapTime - Double(minute) * 60
-                lapTimeLabel.text = String(format:"%02d:%05.2f",minute,second)
-                lapTimes.append(lapTimeLabel.text)
-                laptimeRecordTable.reloadData()
-                lapTime = 0.00
-                lapTimer?.resume()
-            }
-            
-            
-        }else {
-            recordButton.isEnabled = false
-            recordButton.setTitle("Lap", for: .disabled)
-            // 랩타임, 전체시간 기록 라벨 00:00.00으로 초기화
-                        
-            DispatchQueue.main.async {[self] in
-                totalTime = 0.00
-                totalTimeLabel.text = "00:00.00"
-                totalTimer = nil
-            }
-            DispatchQueue.main.async {[self] in
-                lapTime = 0.00
-                lapTimeLabel.text = "00:00.00"
-                lapTimer = nil
-                lapTimes.removeAll()
-                laptimeRecordTable.reloadData()
-            }
+    // MARK: - 타이머를 설정 후 최초 실행시킵니다.
+    func setTimer() {
+        totalTimer = BackgroundTimer(with: 10,handler: {[weak self] in
+            guard let self = self else {return}
+            self.totalTime += 0.01
+            self.setTimeLabel(self.totalTime, self.totalTimeLabel)
+        })
+        lapTimer = BackgroundTimer(with: 10,handler: {[weak self] in
+            guard let self = self else {return}
+            self.lapTime += 0.01
+            self.setTimeLabel(self.lapTime, self.lapTimeLabel)
+        })
+        totalTimer?.activate()
+        lapTimer?.activate()
+    }
     
-            // 테이블 셀 데이터 전부 제거
+    // MARK: - 타이머를 생성 후 실행 또는 일시정지 상태에서 재개시킵니다.
+    func resumeTimer() {
+        lapResetButton.isEnabled = true
+        playPauseButton.setTitle("Stop", for: .normal)
+        playPauseButton.setTitleColor(.systemRed, for: .normal)
+        lapResetButton.setTitle("Lap", for: .normal)
+        
+        if totalTimer == nil {
+            setTimer()
+            return
+        }
+        totalTimer?.resume()
+        lapTimer?.resume()
+    }
+    
+    // MARK: - 실행중인 타이머를 일시정지 시킵니다.
+    func suspendTimer() {
+        playPauseButton.setTitle("Start", for: .normal)
+        playPauseButton.setTitleColor(.systemGreen, for: .normal)
+        lapResetButton.setTitle("Reset", for: .normal)
+        totalTimer?.suspend()
+        lapTimer?.suspend()
+    }
+    
+    // MARK: - 현재 랩 타임을 기록합니다.
+    func recordLapTime() {
+        DispatchQueue.main.async {[self] in
+            lapTimeList.append(lapTimeLabel.text)
+            laptimeRecordTableView.reloadData()
+            lapTime = 0.00
+        }
+    }
+    
+    // MARK: - 스케줄된 타이머를 소멸시키고 연관 UI를 변경합니다.
+    func resetTimer() {
+        lapResetButton.isEnabled = false
+        lapResetButton.setTitle("Lap", for: .disabled)
+        
+        totalTime = 0.00
+        totalTimeLabel.text = "00:00.00"
+        totalTimer = nil
+        
+        lapTime = 0.00
+        lapTimeLabel.text = "00:00.00"
+        lapTimer = nil
+        lapTimeList.removeAll()
+        DispatchQueue.main.async {
+            self.laptimeRecordTableView.reloadData()
+        }
+    }
+    
+    
+    @IBAction func clickPlayPauseButton(_ sender: UIButton) {
+        
+        // MARK: - Start상태의 버튼을 눌렀을 때의 로직
+        if playPauseButton.titleLabel?.text == "Start" {
+            resumeTimer()
             
+        // MARK: - Stop 상태의 버튼을 눌렀을 때의 로직
+        }else {
+            suspendTimer()
+        }
+    }
+    
+    @IBAction func clickLapResetButton(_ sender: UIButton) {
+        
+        // MARK: - Lap상태의 버튼을 눌렀을때의 로직
+        if lapResetButton.titleLabel?.text == "Lap" {
+            recordLapTime()
+            
+        // MARK: - Reset상태의 버튼을 눌렀을 때의 로직
+        }else {
+            resetTimer()
         }
         
     }
@@ -121,19 +136,25 @@ final class ViewController: UIViewController {
     
 }
 
+// MARK: - 랩 타임 기록 테이블 관련 설정
 extension ViewController : UITableViewDataSource {
+    
+    // MARK: - 현재 저장된 랩타임 개수만큼 테이블에 표시합니다.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return lapTimes.count
+        return lapTimeList.count
     }
     
+    // MARK: - cell의 재사용 및 값을 설정합니다.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellId = "cellId"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        // MARK: - 스토리보드에서 설정해 둔 UITableViewCell 내부 UIView의 태그 번호로 구분 및 UI를 설정해줍니다
         if let indexLabel = cell.viewWithTag(1) as? UILabel {
-            indexLabel.text = "Lap \(lapTimes.count - indexPath.row)"
+            indexLabel.text = "Lap \(lapTimeList.count - indexPath.row)"
         }
         if let lapTimeLabel = cell.viewWithTag(2) as? UILabel {
-            lapTimeLabel.text = lapTimes[lapTimes.count - indexPath.row - 1]
+            lapTimeLabel.text = lapTimeList[lapTimeList.count - indexPath.row - 1]
         }
         
         return cell
